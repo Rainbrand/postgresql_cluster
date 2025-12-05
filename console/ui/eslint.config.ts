97c7b6a8f022593772845922c2149e8d4cee0942
Rainbrand <nplskhiv@gmail.com>
2025-12-05 19:53:28 +0300

    refactor: updated eslint to v9 and yarn to v4

---
+++ console/ui/eslint.config.ts
@@ -0,0 +1,33 @@
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import reactHooks from 'eslint-plugin-react-hooks';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    plugins: {
+      pluginReact,
+    },
+    rules: {},
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  tseslint.configs.recommended,
+  reactHooks.configs.flat['recommended-latest'],
+  globalIgnores(['**/dist']),
+]);
